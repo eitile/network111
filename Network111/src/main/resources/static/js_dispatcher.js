3565f99ef62b90b5de938c7594de628390d1ce6b
Yevhen_Shtapauk <yevhen_shtapauk@epam.com>
2017-07-18 22:43:45 +0300

    form values passing to controller with ajax completed.

---
+++ Network111/src/main/resources/static/js_dispatcher.js
@@ -0,0 +1,13 @@
+function sendLoginInfo() {
+    alert("yopta");
+        $.ajax({
+          type: "POST",
+          url: "/login",
+          data: JSON.stringify({
+                'login': $('input[name="login"]').val(),
+                'password': $('input[name="password"]').val()
+          }),
+          dataType: "json",
+          contentType : "application/json; charset=UTF-8"
+        });
+    }
